aeb3c8fcb34c4d4fd5541217c67be576a8e38b48
Mikele-Kochas <michalkocher24@gmail.com>
2024-06-23 17:56:25 +0200

    Update Egzamin.py

--- Egzamin.py
+++ Egzamin.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import random
 
+
 def shuffle_options(question):
     options = question['options']
     correct_answer = question['answer']
@@ -15,6 +16,7 @@ def shuffle_options(question):
         'options': shuffled_options,
         'answer': new_correct_letter
     }
+
 
 def quiz():
     original_questions = [
@@ -123,42 +125,20 @@ def quiz():
             "answer": "B"
         },
         {
+            "question": "Jakie są metody wyszukiwania danych w systemie informatycznym i jakie są ich cechy charakterystyczne?",
+            "options": ["A) Przechowywanie i zabezpieczanie danych",
+                        "B) Zbieranie danych o istotnych parametrach organizacyjnych",
+                        "C) Sortowanie i przetwarzanie danych",
+                        "D) Sposoby przeszukiwania danych w celu uzyskania odpowiedzi"],
+            "answer": "D"
+        },
+        {
             "question": "Jakie są główne zadania funkcji ewidencji danych w systemie informatycznym?",
             "options": ["A) Zbieranie danych o istotnych parametrach organizacyjnych",
                         "B) Wyszukiwanie i wyprowadzanie danych",
                         "C) Przechowywanie i zabezpieczanie danych",
                         "D) Różnorodne operacje na danych, np. sortowanie, obliczenia"],
             "answer": "A"
-        },
-        {
-            "question": "Czym charakteryzują się systemy ewidencyjne?",
-            "options": [
-                "A) Analizują strategiczne problemy decyzyjne",
-                "B) Tworzą zbiory elementarnych danych o funkcjonowaniu obiektu",
-                "C) Informują o stanie obiektu i jego poszczególnych parametrach",
-                "D) Dokonują inteligentnej analizy danych"
-            ],
-            "correct_answer": "B"
-        },
-        {
-            "question": "Jakie zadanie mają systemy informowania kierownictwa?",
-            "options": [
-                "A) Informują o stanie obiektu i jego poszczególnych parametrach",
-                "B) Tworzą zbiory elementarnych danych o funkcjonowaniu obiektu",
-                "C) Analizują strategiczne problemy decyzyjne",
-                "D) Dokonują inteligentnej analizy danych"
-            ],
-            "correct_answer": "A"
-        },
-        {
-            "question": "Czym charakteryzują się systemy ekspertowe?",
-            "options": [
-                "A) Tworzą zbiory elementarnych danych o funkcjonowaniu obiektu",
-                "B) Informują o stanie obiektu i jego poszczególnych parametrach",
-                "C) Analizują strategiczne problemy decyzyjne",
-                "D) Są 'samouczące się', dokonują inteligentnej analizy danych"
-            ],
-            "correct_answer": "D"
         },
         {
             "question": "Co obejmuje funkcja przekształcania danych w systemie informatycznym?",
@@ -187,7 +167,8 @@ def quiz():
     ]
 
     if 'shuffled_questions' not in st.session_state:
-        st.session_state.shuffled_questions = [shuffle_options(q) for q in random.sample(original_questions, len(original_questions))]
+        st.session_state.shuffled_questions = [shuffle_options(q) for q in
+                                               random.sample(original_questions, len(original_questions))]
 
     if 'score' not in st.session_state:
         st.session_state.score = 0
@@ -210,7 +191,8 @@ def quiz():
             question = st.session_state.shuffled_questions[st.session_state.question_index]
             st.write(f"\n**Pytanie {st.session_state.question_index + 1}:** {question['question']}")
 
-            options = [f"{chr(65 + i)}) {option.split(')', 1)[1].strip()}" for i, option in enumerate(question['options'])]
+            options = [f"{chr(65 + i)}) {option.split(')', 1)[1].strip()}" for i, option in
+                       enumerate(question['options'])]
             user_answer = st.radio("Twoja odpowiedź:", options, key=str(st.session_state.question_index))
 
             if st.button("Następne pytanie"):
@@ -221,10 +203,12 @@ def quiz():
                 st.experimental_rerun()
 
         else:
-            st.write(f"\n**Quiz zakończony! Twój wynik to:** {st.session_state.score}/{len(st.session_state.shuffled_questions)}")
+            st.write(
+                f"\n**Quiz zakończony! Twój wynik to:** {st.session_state.score}/{len(st.session_state.shuffled_questions)}")
 
             st.write("\n**Lista poprawnych odpowiedzi:**")
-            for idx, (question, user_answer) in enumerate(zip(st.session_state.shuffled_questions, st.session_state.user_answers)):
+            for idx, (question, user_answer) in enumerate(
+                    zip(st.session_state.shuffled_questions, st.session_state.user_answers)):
                 correct_option = question['options'][ord(question['answer']) - ord('A')]
                 is_correct = user_answer.startswith(question['answer'])
 
@@ -238,8 +222,10 @@ def quiz():
                 st.session_state.score = 0
                 st.session_state.question_index = 0
                 st.session_state.user_answers = []
-                st.session_state.shuffled_questions = [shuffle_options(q) for q in random.sample(original_questions, len(original_questions))]
+                st.session_state.shuffled_questions = [shuffle_options(q) for q in
+                                                       random.sample(original_questions, len(original_questions))]
                 st.experimental_rerun()
+
 
 if __name__ == "__main__":
     quiz()
