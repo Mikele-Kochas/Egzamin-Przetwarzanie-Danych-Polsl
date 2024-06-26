dc83abdb4edb0d5b492ca86d623496d9e2d47732
Mikele-Kochas <michalkocher24@gmail.com>
2024-06-26 11:57:58 +0200

    Update Egzamin.py

--- Egzamin.py
+++ Egzamin.py
@@ -230,7 +230,334 @@ def quiz():
         }
     ]
 
+    additional_questions_2 = [
+        {
+            "question": "Połącz skrót PCA z polską nazwą techniki:",
+            "options": [
+                "A) Analiza składowych głównych",
+                "B) Analiza czynnikowa",
+                "C) Rozkład macierzy nieujemnej",
+                "D) Analiza głównych kierunków"
+            ],
+            "answer": "A"
+        },
+        {
+            "question": "Połącz skrót FA z polską nazwą techniki:",
+            "options": [
+                "A) Analiza składowych głównych",
+                "B) Analiza czynnikowa",
+                "C) Rozkład macierzy nieujemnej",
+                "D) Analiza czynnikowa"
+            ],
+            "answer": "D"
+        },
+        {
+            "question": "Połącz skrót NMF z polską nazwą techniki:",
+            "options": [
+                "A) Analiza składowych głównych",
+                "B) Analiza czynnikowa",
+                "C) Rozkład macierzy nieujemnej",
+                "D) Niedodatni rozkład macierzy"
+            ],
+            "answer": "D"
+        }
+    ]
+    additional_questions_3 = [
+        {
+            "question": "Co to jest dana?",
+            "options": ["A) Interpretacja przetworzonych informacji",
+                        "B) Sformalizowana wiadomość o określonej strukturze",
+                        "C) Wiedza nadająca się do użycia",
+                        "D) Sygnał dotyczący zjawiska lub procesu"],
+            "answer": "B"
+        },
+        {
+            "question": "Co to jest informacja?",
+            "options": ["A) Sformalizowana wiadomość o określonej strukturze",
+                        "B) Powstaje na skutek interpretacji przetworzonych danych",
+                        "C) Sygnał dotyczący zjawiska lub procesu",
+                        "D) Zbiór obiektów przetwarzających dane"],
+            "answer": "B"
+        },
+        {
+            "question": "Która definicja najlepiej opisuje wiedzę?",
+            "options": ["A) Surowa, sformalizowana wiadomość",
+                        "B) Informacje nadające się do użycia",
+                        "C) Sygnał dotyczący zjawiska lub procesu",
+                        "D) Zbiór danych o określonej strukturze"],
+            "answer": "B"
+        },
+        {
+            "question": "Co to jest dyspozycyjność w kontekście kryteriów oceny jakości informacji?",
+            "options": ["A) Wiarygodność informacji w danym momencie",
+                        "B) Dokładność i poprawność informacji",
+                        "C) Możliwość dostępu do każdej informacji w systemie w dowolnym momencie",
+                        "D) Zdolność do przeprowadzenia analizy porównawczej"],
+            "answer": "C"
+        },
+        {
+            "question": "Czym charakteryzuje się system informacyjny?",
+            "options": ["A) Jest to wyłącznie zbiór urządzeń komputerowych",
+                        "B) Obejmuje tylko procesy przetwarzania danych",
+                        "C) To zbiór obiektów przetwarzających informacje, ludzi, danych i procesów",
+                        "D) Dotyczy tylko infrastruktury sieciowej"],
+            "answer": "C"
+        },
+        {
+            "question": "Co to jest system informatyczny?",
+            "options": ["A) Zbiór obiektów przetwarzających informacje",
+                        "B) Część systemu informacyjnego realizująca przetwarzanie informacji za pomocą systemów komputerowych",
+                        "C) Zbiór ludzi, danych i procesów",
+                        "D) Infrastruktura sieciowa i urządzenia komputerowe"],
+            "answer": "B"
+        },
+        {
+            "question": "Która z poniższych NIE jest funkcją systemu informatycznego?",
+            "options": ["A) Ewidencja danych",
+                        "B) Przekształcanie danych",
+                        "C) Interpretacja emocjonalna danych",
+                        "D) Wyszukiwanie i wyprowadzanie danych"],
+            "answer": "C"
+        },
+        {
+            "question": "Które z poniższych jest przykładem zmiennej jakościowej?",
+            "options": ["A) Liczba studentów na wykładzie",
+                        "B) Długość w metrach",
+                        "C) Nazwa uczelni (np. AWF, AGH)",
+                        "D) Temperatura powietrza w stopniach Celsjusza"],
+            "answer": "C"
+        },
+        {
+            "question": "Co to jest zmienna ilorazowa?",
+            "options": ["A) Zmienna przyjmująca tylko dwie wartości",
+                        "B) Zmienna mierzona w skali, np. długość w metrach",
+                        "C) Zmienna określająca kolejność",
+                        "D) Zmienna o nieuporządkowanych wartościach"],
+            "answer": "B"
+        },
+        {
+            "question": "Która z poniższych jest przykładem zmiennej binarnej?",
+            "options": ["A) Wiek osoby",
+                        "B) Ocena z egzaminu (2-5)",
+                        "C) Prawda/fałsz",
+                        "D) Nazwa miesiąca"],
+            "answer": "C"
+        },
+        {
+            "question": "Co to jest progowanie?",
+            "options": ["A) Ograniczenie liczby wartości zmiennej do 2",
+                        "B) Przekształcenie danych do tej samej skali",
+                        "C) Zastąpienie zbioru wartości etykietą określającą przedział wartości",
+                        "D) Przekształcanie danych do rozkładu o średniej 0 i odchyleniu standardowym 1"],
+            "answer": "C"
+        },
+        {
+            "question": "Co to jest binaryzacja?",
+            "options": ["A) Przekształcenie danych do tej samej skali",
+                        "B) Ograniczenie liczby wartości zmiennej do 2",
+                        "C) Zastąpienie zbioru wartości etykietą określającą przedział wartości",
+                        "D) Przekształcanie danych do rozkładu o średniej 0"],
+            "answer": "B"
+        },
+        {
+            "question": "Która metoda przekształca dane do rozkładu o średniej 0 i odchyleniu standardowym 1?",
+            "options": ["A) Normalizacja",
+                        "B) Binaryzacja",
+                        "C) Progowanie",
+                        "D) Standaryzacja"],
+            "answer": "D"
+        },
+        {
+            "question": "Co charakteryzuje rozkład skośny?",
+            "options": ["A) Symetryczna dystrybucja",
+                        "B) Niesymetryczna dystrybucja przypominająca rozkład normalny",
+                        "C) Zawsze ma ogon skierowany w lewo",
+                        "D) Ma zawsze średnią równą 0"],
+            "answer": "B"
+        },
+        {
+            "question": "Co oznacza pojęcie wielowymiarowości w analizie danych?",
+            "options": ["A) Analiza danych wyłącznie w trzech wymiarach",
+                        "B) Sytuacja, gdy analizujemy dane charakteryzujące się wieloma zmiennymi",
+                        "C) Badanie tylko nieskorelowanych zmiennych",
+                        "D) Analiza wyłącznie zmiennych ilościowych"],
+            "answer": "B"
+        },
+        {
+            "question": "Która metoda redukcji wymiarów zastępuje zbiór zmiennych mniejszym zbiorem ich kombinacji liniowych?",
+            "options": ["A) Analiza składowych głównych (PCA)",
+                        "B) Analiza czynnikowa (FA)",
+                        "C) Rozkład macierzy nieujemnej (NMF)",
+                        "D) Jednoklasowa maszyna wektorów nośnych (One-class SVM)"],
+            "answer": "A"
+        },
+        {
+            "question": "Co to jest analiza czynnikowa (FA)?",
+            "options": ["A) Metoda redukcji wymiarów oparta na liniowej kombinacji zmiennych",
+                        "B) Technika wykrywania anomalii",
+                        "C) Sposób normalizacji danych",
+                        "D) Metoda imputacji brakujących wartości"],
+            "answer": "A"
+        },
+        {
+            "question": "Co to jest korelacja?",
+            "options": ["A) Miara centralnej tendencji danych",
+                        "B) Sposób normalizacji danych",
+                        "C) Unormowana kowariancja",
+                        "D) Metoda redukcji wymiarów"],
+            "answer": "C"
+        },
+        {
+            "question": "Czym charakteryzuje się detektor anomalii oparty na gęstości rozkładu?",
+            "options": ["A) Zakłada, że normalne obserwacje umieszczone są gęsto, a anomalie leżą w oddaleniu",
+                        "B) Bazuje wyłącznie na grupowaniu danych",
+                        "C) Wykorzystuje funkcje jądrowe do separacji danych",
+                        "D) Jest oparty na sieciach neuronowych"],
+            "answer": "A"
+        },
+        {
+            "question": "Która metoda wykrywania anomalii wykorzystuje kolekcję drzew izolacyjnych?",
+            "options": ["A) K-Nearest Neighborhood",
+                        "B) Local Outlier Factor",
+                        "C) One-class SVM",
+                        "D) Isolation Forest (Las izolacyjny)"],
+            "answer": "D"
+        },
+        {
+            "question": "Która metoda redukcji szumów w obrazie wykorzystuje wzory z okolicznych pikseli?",
+            "options": ["A) Median filter (Filtr medianowy)",
+                        "B) Non-Local Means Denoising (NLMeans)",
+                        "C) Gaussian filter (Filtr Gaussa)",
+                        "D) Bilateral filter (Filtr bilateralny)"],
+            "answer": "B"
+        },
+        {
+            "question": "Który filtr wygładza obraz, zachowując krawędzie?",
+            "options": ["A) Median filter (Filtr medianowy)",
+                        "B) Non-Local Means Denoising (NLMeans)",
+                        "C) Gaussian filter (Filtr Gaussa)",
+                        "D) Bilateral filter (Filtr bilateralny)"],
+            "answer": "D"
+        },
+        {
+            "question": "Co to jest wariancja?",
+            "options": ["A) Miara centralnej tendencji danych",
+                        "B) Miara rozrzutu danych",
+                        "C) Miara symetrii rozkładu",
+                        "D) Miara korelacji między zmiennymi"],
+            "answer": "B"
+        },
+        {
+            "question": "Co mierzy test ANOVA?",
+            "options": ["A) Symetrię rozkładu",
+                        "B) Różnice średnich między grupami",
+                        "C) Stopień korelacji",
+                        "D) Rozkład danych"],
+            "answer": "B"
+        },
+        {
+            "question": "Który test służy do porównań post hoc po wykonaniu ANOVA?",
+            "options": ["A) Test HSD Tukeya",
+                        "B) Test t-Studenta",
+                        "C) Test Chi-kwadrat",
+                        "D) Test Wilcoxona"],
+            "answer": "A"
+        },
+        {
+            "question": "Która metoda wykrywania anomalii wykorzystuje sieci neuronowe do modelowania danych?",
+            "options": ["A) K-Nearest Neighborhood",
+                        "B) Local Outlier Factor",
+                        "C) Autodekodery (Autoencoders)",
+                        "D) Isolation Forest (Las izolacyjny)"],
+            "answer": "C"
+        },
+        {
+            "question": "Która metoda regularyzacji dodaje karę za sumę wartości bezwzględnych współczynników?",
+            "options": ["A) Regresja LASSO",
+                        "B) Regresja Ridge",
+                        "C) Elastic Net",
+                        "D) Principal Component Regression"],
+            "answer": "A"
+        },
+        {
+            "question": "Która metoda wykrywania anomalii opiera się na podobieństwie lokalnym?",
+            "options": ["A) Isolation Forest",
+                        "B) One-class SVM",
+                        "C) Local Outlier Factor",
+                        "D) Autodekodery"],
+            "answer": "C"
+        },
+        {
+            "question": "Która metoda imputacji brakujących danych polega na zastąpieniu brakujących wartości średnią lub medianą?",
+            "options": ["A) Imputacja wielokrotna",
+                        "B) Uzupełnianie braków przez predykcję",
+                        "C) Imputacja prostą średnią lub medianą",
+                        "D) Usuwanie braków"],
+            "answer": "C"
+        },
+        {
+            "question": "Która metoda imputacji brakujących danych zastępuje brakujące wartości najbardziej prawdopodobną wartością na podstawie innych danych?",
+            "options": ["A) Imputacja prostą średnią lub medianą",
+                        "B) Uzupełnianie braków przez predykcję",
+                        "C) Usuwanie braków",
+                        "D) Imputacja wielokrotna"],
+            "answer": "B"
+        },
+        {
+            "question": "Która technika próbkuje dane w taki sposób, aby zapewnić reprezentatywność próby?",
+            "options": ["A) Próbkowanie losowe",
+                        "B) Próbkowanie warstwowe",
+                        "C) Próbkowanie systematyczne",
+                        "D) Próbkowanie nielosowe"],
+            "answer": "B"
+        },
+        {
+            "question": "Która technika próbkuje dane w równych odstępach?",
+            "options": ["A) Próbkowanie losowe",
+                        "B) Próbkowanie warstwowe",
+                        "C) Próbkowanie systematyczne",
+                        "D) Próbkowanie nielosowe"],
+            "answer": "C"
+        },
+        {
+            "question": "Czym charakteryzuje się kroswalidacja?",
+            "options": ["A) Wykorzystuje cały zbiór danych do trenowania i testowania",
+                        "B) Podział danych na podzbiory, na których naprzemiennie trenuje się i testuje model",
+                        "C) Wykorzystanie tylko części danych do trenowania",
+                        "D) Wykonywanie analizy na wszystkich dostępnych danych bez podziału"],
+            "answer": "B"
+        },
+        {
+            "question": "Która metoda testowania modeli polega na iteracyjnym usuwaniu jednego elementu z próby i trenowaniu modelu na pozostałych?",
+            "options": ["A) Kroswalidacja",
+                        "B) Bootstrap",
+                        "C) Próbkowanie warstwowe",
+                        "D) Metoda jackknife"],
+            "answer": "D"
+        },
+        {
+            "question": "Która technika wykrywania anomalii wykorzystuje minimalną przestrzeń zamknięcia?",
+            "options": ["A) K-Nearest Neighborhood",
+                        "B) Local Outlier Factor",
+                        "C) One-class SVM",
+                        "D) Isolation Forest (Las izolacyjny)"],
+            "answer": "C"
+        },
+        {
+            "question": "Która technika jest używana do redukcji szumów w obrazach i jest bardziej zaawansowana od filtra medianowego?",
+            "options": ["A) Non-Local Means Denoising (NLMeans)",
+                        "B) Gaussian filter (Filtr Gaussa)",
+                        "C) Bilateral filter (Filtr bilateralny)",
+                        "D) Median filter (Filtr medianowy)"],
+            "answer": "A"
+        }
+    ]
+
     original_questions.extend(additional_questions)
+
+    original_questions.extend(additional_questions_2)
+
+    original_questions.extend(additional_questions_3)
 
     if 'shuffled_questions' not in st.session_state:
         st.session_state.shuffled_questions = [shuffle_options(q) for q in
